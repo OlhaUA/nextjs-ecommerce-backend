ad086217f0a33dc983b7fe6334a37e877aa486c1
OlhaUA <olhaanhelua@gmail.com>
2021-07-05 18:44:17 +0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '72dd25674304f8f59f3f1f705919123c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '213171ee-e2c5-4117-96a6-bbd4b3ce44f8'
+};
